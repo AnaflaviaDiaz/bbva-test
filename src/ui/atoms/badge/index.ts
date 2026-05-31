e2739198779ceb82dc7a2be8de2cc5bf07ef6d37
AnaflaviaDiaz <anaflaviadiazmartel5a@gmail.com>
2026-05-31 20:00:46 +0200

    feat: create badge component and its storybook

---
+++ src/ui/atoms/badge/index.ts
@@ -0,0 +1,40 @@
+import { css, html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+
+import { Variant } from '../../models/variant.js';
+
+@customElement('bbva-badge')
+export class Badge extends LitElement {
+  static styles = css`
+    .badge__primary {
+      background-color: #001391;
+      color: #fff;
+    }
+
+    .badge__primary-light {
+      background-color: #0c6dff;
+      color: #fff;
+    }
+
+    .badge__secondary {
+      color: #070e46;
+      background-color: #f7f8f8;
+    }
+
+    .badge {
+      display: inline-block;
+      padding: 0 8px;
+      border-radius: 16px;
+    }
+  `;
+
+  @property({ type: String }) text: string = '';
+
+  @property({ type: String }) variant: Variant = 'secondary';
+
+  render() {
+    return html`
+      <span class="badge badge__${this.variant}">${this.text}</span>
+    `;
+  }
+}
---
+++ stories/Badge.stories.ts
@@ -0,0 +1,50 @@
+import { html, TemplateResult } from 'lit';
+
+import '../src/ui/atoms/badge/index.js';
+import { Variant } from '../src/ui/models/variant.js';
+
+export default {
+  title: 'Badge',
+  component: 'bbva-badge',
+  argTypes: {
+    text: { control: 'text' },
+    variant: {
+      control: { type: 'select' },
+      options: ['primary', 'secondary', 'primary-light'],
+    },
+  },
+  parameters: {
+    a11y: {
+      config: {
+        rules: [{ id: 'color-contrast', enabled: true }],
+      },
+    },
+  },
+};
+
+interface Story<T> {
+  (args: T): TemplateResult;
+  args?: Partial<T>;
+  argTypes?: Record<string, unknown>;
+}
+
+interface ArgTypes {
+  text: string;
+  variant?: Variant;
+}
+
+const Template: Story<ArgTypes> = ({
+  text = 'Hello world',
+  variant = 'primary',
+}: ArgTypes) => html`
+  <bbva-badge
+    .text=${text}
+    .variant=${variant}
+  ></bbva-badge>
+`;
+
+export const Default = Template.bind({});
+Default.args = {
+  text: 'Nuevo',
+  variant: 'primary',
+};

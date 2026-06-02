52311036d9074cbaa3e983c2c7db8b401abc690a
AnaflaviaDiaz <anaflaviadiazmartel5a@gmail.com>
2026-06-02 19:31:36 +0200

    feat(date-formatter): update test-change date result and update test

--- src/utils/date-formatter.test.ts
+++ src/utils/date-formatter.test.ts
@@ -3,14 +3,26 @@ import { dateFormatter } from './date-formatter.js';
 
 describe('dateFormatter', () => {
   it('should format date when month is lower than 10', () => {
-    const result = dateFormatter(new Date('1997-09-25'));
+    const result = dateFormatter('1997-09-25');
 
-    expect(result).to.equal('25-09-1997');
+    expect(result).to.equal('25/09/1997');
   });
 
   it('should format date when day is lower than 10', () => {
-    const result = dateFormatter(new Date('2026-06-05'));
+    const result = dateFormatter('2026-06-05');
 
-    expect(result).to.equal('05-06-2026');
+    expect(result).to.equal('05/06/2026');
+  });
+
+  it('should format date when month is higher than 10', () => {
+    const result = dateFormatter('2026-11-05');
+
+    expect(result).to.equal('05/11/2026');
+  });
+
+  it('should return error when date is invalid', () => {
+    const result = dateFormatter('invalid-date');
+
+    expect(result).to.equal('Invalid Date');
   });
 });
--- src/utils/date-formatter.ts
+++ src/utils/date-formatter.ts
@@ -1,10 +1,25 @@
-export const dateFormatter = (date: Date): string => {
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const yyyy = date.getFullYear();
+/**
+ * Formatea la fecha en formato 'DD/MM/YYYY'.
+ * Si el día o el mes es menor que 10, se agrega un cero delante.
+ * @param date - fecha con formato 'YYY-MM-DD'
+ * @returns fecha formateada con formato 'DD/MM/YYYY' o 'Invalid Date' si la fecha es inválida
+ * @example
+ * dateFormatter('2026-06-05'); => '05/06/2026'
+ * dateFormatter('invalid-date'); => 'Invalid Date'
+ */
+export const dateFormatter = (date: string): string => {
+  const newDate = new Date(date);
+
+  if (Number.isNaN(newDate.getTime())) {
+    return 'Invalid Date';
+  }
+
+  const day = newDate.getDate();
+  const month = newDate.getMonth() + 1;
+  const yyyy = newDate.getFullYear();
 
   const dd = `${day < 10 ? '0' : ''}${day}`;
   const mm = `${month < 10 ? '0' : ''}${month}`;
 
-  return `${dd}-${mm}-${yyyy}`;
+  return `${dd}/${mm}/${yyyy}`;
 };

1b23c3dbe8b0f5dd72e35fd65faa3fdc3a148070
AnaflaviaDiaz <anaflaviadiazmartel5a@gmail.com>
2026-06-02 19:32:07 +0200

    feat(amount-formatter): add jsdcoc comment

--- src/utils/amount-formatter.ts
+++ src/utils/amount-formatter.ts
@@ -4,7 +4,25 @@ interface AmountConfig {
   locale?: Intl.LocalesArgument;
 }
 
-export const amountFormatter = ({ locale = 'de-DE', currency, amount }: AmountConfig) =>
+/**
+ * Formatea el monto con moneda utlizando la configuración regional enviadas.
+ * @param config - configuración para formatear el monto
+ * @param config.locale - localización para formatear el monto, ej: 'es-ES' | 'en-US' | etc. Si no se envía, se utiliza 'de-DE' por defecto.
+ * @param config.currency - moneda para formatear el monto, por ejemplo 'EUR' | 'USD' | etc.
+ * @param config.amount - monto a formatear
+ * @returns { string } monto formateado con moneda
+ * @example
+ * amountFormatter({
+      locale: 'de-DE',
+      currency: 'EUR',
+      amount: 7_885.23,
+    }); => '7.885,23 €'
+ */
+export const amountFormatter = ({
+  locale = 'de-DE',
+  currency,
+  amount,
+}: AmountConfig): string =>
   new Intl.NumberFormat(locale, {
     style: 'currency',
     currency,
